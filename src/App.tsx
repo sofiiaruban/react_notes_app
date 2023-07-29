b1d16bab52422eeb8a2b2428e23a4e3dcfdfeb4d
Sofia Ruban <sophiaruban249324@gmail.com>
2023-07-29 21:02:21 +0300

    build: Install types for redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
 import { HashRouter, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import NotePage from './pages/NotePage'
 function App() {
 
   return (
     <>
       <HashRouter>
         <Routes>
-          <Route path="/"></Route>
-          <Route path="/note"></Route>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/note" element={<NotePage />} />
           <Route path="/note/:id"></Route>
         </Routes>
       </HashRouter>
