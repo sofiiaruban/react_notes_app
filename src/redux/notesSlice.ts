9392fe3e6ca51b096c3d2e1e070e9b38bae7488a
Sofia Ruban <sophiaruban249324@gmail.com>
2023-07-29 21:20:11 +0300

    feat(noteSlice): Add initial notes data

--- src/redux/notesSlice.ts
+++ src/redux/notesSlice.ts
@@ -9,3 +9,47 @@ interface Note {
   content: string
   dates: string[]
 }
+const initialNotes:Note[] = [
+  {
+    name: 'Shopping List',
+    created: '7/26/2023',
+    category: 'Task',
+    content: 'eggs, pasta, butter,salt',
+    dates: ['7/26/2023']
+  },
+  {
+    name: 'Thought',
+    created: '7/26/2023',
+    category: 'Random Thought',
+    content: 'donec pretium vulputate',
+    dates: ['7/26/2023']
+  },
+  {
+    name: 'Bagfix',
+    created: '7/26/2023',
+    category: 'Idea',
+    content: 'tincidunt ornare massa eget',
+    dates: ['7/26/2023', '7/28/2023']
+  },
+  {
+    name: 'Shopping List',
+    created: '7/26/2023',
+    category: 'Task',
+    content: 'tincidunt ornare massa eget',
+    dates: ['7/29/2023']
+  },
+  {
+    name: 'Ornare Lectus',
+    created: '7/26/2023',
+    category: 'Random Thought',
+    content: 'pellentesque massa placerat duis',
+    dates: ['7/26/2023']
+  },
+  {
+    name: 'Rhoncus Mattis',
+    created: '7/26/2023',
+    category: 'Idea',
+    content: 'magna etiam tempor orci',
+    dates: ['7/26/2023']
+  }
+]
