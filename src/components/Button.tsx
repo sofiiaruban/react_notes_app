cf5091c6cc87421f31150581664ef061e2204107
Sofia Ruban <sophiaruban249324@gmail.com>
2023-07-30 19:57:17 +0300

    style(button): Add button css module

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
 import { ButtonProps } from "../types/ButtonProps"
+import styles from "./Button.module.css"
 
 const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
-  return <button onClick={onClick}>{children}</button>
+  return <button onClick={onClick} className={styles.button}>{children}</button>
 }
 export default Button
