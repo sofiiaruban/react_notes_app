cac5196f21fc3e2c383be419024dc93de22b7cfb
Sofia Ruban <sophiaruban249324@gmail.com>
2023-07-30 20:58:01 +0300

    feat(notepage): Add categories and summary data

--- src/pages/NotePage.tsx
+++ src/pages/NotePage.tsx
@@ -1,5 +1,12 @@
 
-const NotePage =()=> {
+import { useSelector } from 'react-redux'
+import { RootState } from '../redux/store'
+
+const NotePage: React.FC = () => {
+  const initialNote = useSelector((state: RootState) => state.notes.notes[0])
+  const summary = useSelector((state: RootState) => state.notes.summary)
+  const categories = Object.keys(summary)
+  const inputNames = Object.keys(initialNote)
   return <div>Note</div>
 }
 export default NotePage;
