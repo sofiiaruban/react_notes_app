a4aec20035bcacf3ac973f3539b5ef121f5a2964
Sofia Ruban <sophiaruban249324@gmail.com>
2023-07-30 14:25:58 +0300

    feat(table): Add getTableHeadNames func

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,10 +1,22 @@
 import { TableProps } from "../types/TableProps";
 
 const Table: React.FC<TableProps> = ({ data }) => {
+  
+  const getTableHeadNames = () => {
+    let tableHeadNames
+
+    if (Array.isArray(data)) {
+      tableHeadNames = Object.keys(data[0])
+    } else {
+      tableHeadNames = Object.keys(data)
+    }
+    
+    return tableHeadNames
+  } 
+
   return (
     <table>
-      {JSON.stringify(data)}
-      <thead></thead>
+      <thead>{JSON.stringify(getTableHeadNames())}</thead>
       <tbody></tbody>
     </table>
   )
